5e190a69a300e3bdb91e24428985d5da09ec38b7
Bumble <bumble.sakh@gmail.com>
2021-12-11 02:47:41 +1100

    add: initApp and get prices

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,40 @@
 import './plugins';
 import locations from './store/locations';
 import '../scss/style.scss';
+import formUI from '../js/views/form';
+import currencyUI from './views/currency';
 
-locations.init().then(res => {
-    console.log(res)
-    console.log(locations);
-    console.log(locations.getCitiesByCountryCode("US"));
+document.addEventListener('DOMContentLoaded', () => {
+    initApp();
+    const form = formUI.form;
+
+    // Events
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        onFormSubmit();
+    })
+
+    // Handlers
+    async function initApp() {
+        await locations.init();
+        formUI.setData(locations.shortCitiesList);
+    }
+
+    async function onFormSubmit() {
+        // Собрать данные формы
+        const origin = locations.getCityCodeByKey(formUI.originValue);
+        const destination = locations.getCityCodeByKey(formUI.destinationValue);
+        const depart_date = locations.getShortDate(formUI.departValue);
+        const return_date = locations.getShortDate(formUI.returnValue);
+        const currency = currencyUI.currencyValue;
+
+        await locations.fetchTickets({
+            origin,
+            destination,
+            depart_date,
+            return_date,
+            currency,
+        });
+    }    
+
 });
