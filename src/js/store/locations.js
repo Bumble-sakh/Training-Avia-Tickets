1f070ebe6c54dc2e8c0625f4e32bb915d8af78e5
Bumble <bumble.sakh@gmail.com>
2021-12-11 02:44:59 +1100

    add: methods for working with date

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -5,6 +5,7 @@ class Locations {
         this.api = api;
         this.countries = null;
         this.cities = null;
+        this.shortCitiesList = null;
     }
     async init() {
         const response = await Promise.all([
@@ -13,15 +14,56 @@ class Locations {
         ])
 
         const [countries, cities] = response;
-        this.countries = countries.data;
-        this.cities = cities.data;
+        this.countries = this.serializeCountries(countries.data);
+        this.cities = this.serializeCities(cities.data);
+        this.shortCitiesList = this.createShortCitiesList(this.cities)
 
         return response;
     }
-    getCitiesByCountryCode(code) {
-        return this.cities.filter( city => {
-            return city.country_code == code;
-        });
+
+    getCityCodeByKey(key) {
+        return this.cities[key].code;
+    }
+
+    getShortDate(date) {
+        return date.split('').slice(0, -3).join('');
+    }
+
+    createShortCitiesList(cities) {
+        return Object.entries(cities).reduce((acc, [city_name, city]) => {
+            const data = {
+                label: city_name,
+                value: city.code,
+            }
+            acc.push(data)
+            return acc;
+        }, []);
+    }
+
+    serializeCountries(countries) {
+        return countries.reduce((acc, country) => {
+            acc[country.code] = country;
+            return acc;
+        }, {})
+    }
+
+    serializeCities(cities) {
+        return cities.reduce((acc, city) => {
+            const country_name = this.getCountryNameByCode(city.country_code);
+            const city_name = city.name || city.name_translations.en;
+            const key = `${city_name}, ${country_name}`
+            acc[key] = city;
+            return acc;
+        }, {})
+    }
+
+    getCountryNameByCode(code) {
+        return this.countries[code].name;
+    }
+
+    async fetchTickets(params) {
+        const response = await this.api.prices(params);
+        console.log(response);
     }
 }
 
